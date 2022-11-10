f05b9b2a10f7466791b3da45ef0777c840623a87
AlexKuro <89215682813@mail.ru>
2022-11-10 23:21:46 +0300

    Example_004_if_else_MASHA

---
+++ Example_Lesson_1/Example_004_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿string nameRandom;
+Console.WriteLine("Игра!");
+Console.WriteLine("Кто из нас самый лучший!");
+Console.WriteLine("Введите Ваше И М Я!!!");
+nameRandom = Console.ReadLine();
+if (nameRandom.ToLower() == "маша")
+{
+    Console.WriteLine("Урааа! Ты самая лучшая!!!");
+}
+else
+{
+    Console.WriteLine("Просто здравствуй - " + nameRandom);           
+}
