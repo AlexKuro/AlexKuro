bf40057e16f7ceedc7a98ac3f15529eb36aa4dea
AlexKuro <89215682813@mail.ru>
2022-11-10 23:03:52 +0300

    Example_003_SUM

---
+++ Example_Lesson_1/Example_003_sum/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double in_3, in_4, result_2;
+in_3 = new Random().Next(1,10); //2,3,4,5,6,7,8,9
+in_4 = new Random().Next(1,10); //2,3,4,5,6,7,8,9
+result_2 = in_3 / in_4;
+Console.Write(in_3);
+Console.Write(" / ");
+Console.Write(in_4);
+Console.Write(" = ");
+Console.Write(result_2);
