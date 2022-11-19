3075a98fcc5316714e7064fcf7b22b6a4fba3eaf
AlexKuro <89215682813@mail.ru>
2022-11-19 13:09:29 +0300

    Lesson_2_Example_002_003 Array

---
+++ Example_Lesson_2/Example_002_array/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int a, int b, int c)
+{
+    int max = a; 
+    if (b > max) max = b; 
+    if (c > max) max = c;
+    return max;
+}
+//               0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 35, 26, 28, 24, 15, 48, 50};
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
